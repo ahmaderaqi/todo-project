f01ad343d46b4459b89f80db446c2a7adf3fe258
ahmaderaqi <ahmadzaher573@gmail.com>
2023-02-08 13:42:58 +0000

    modify

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ var name1 = prompt("Insert your name");
 var gender = prompt("insert your gender");
 var age = prompt("insert your age");
 
-if(age<=0){
+if (age <= 0) {
     alert("wrong age, it should'n be negative or zero");
     age = prompt("insert your age again");
 }
+var confirmation = confirm("do you want to skip the welcoming message?");
+if (!confirmation) {
+    if (gender == "male") {
+        alert("welcome mr. " + name1);
+    }
+    else if (gender == "female") {
+        alert("welcome ms. " + name1);
+    }
+    else {
+        alert("welcome " + name1);
+    }
+}
 
-if(gender=="male"){
-    alert("welcome mr. "+ name1);
-}
-else if(gender=="female"){
-    alert("welcome ms. "+ name1);
-}
-else{
-    alert("welcome " + name1);
-}
+
+console.log(confirmation);
 
